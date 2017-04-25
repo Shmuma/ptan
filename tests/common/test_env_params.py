1f27f40de042e0cf4c3d048a71479e7bf1380dbb
Max Lapan <maxl@fornova.net>
2017-04-25 11:26:36 +0200

    Tests

---
+++ tests/common/test_env_params.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+import gym
+from ptan.common import env_params
+
+
+class TestEnvParams(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.env = gym.make("Acrobot-v1")
+
+    def test_params(self):
+        params = env_params.EnvParams(self.env)
+        self.assertEqual(3, params.n_actions)
+        self.assertEqual((6,), params.state_shape)
+
+    def test_register(self):
+        self.assertIsNone(env_params.get())
+        params = env_params.EnvParams(self.env)
+        env_params.register(params)
+        self.assertEqual(params, env_params.get())
