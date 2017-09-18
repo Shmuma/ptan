75b03a323d469788d2b0c322b74443ac6596f7b9
Max Lapan <max.lapan@gmail.com>
2017-09-18 16:39:15 +0200

    Extra fields in setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ import setuptools
 
 setuptools.setup(
     name="ptan",
+    author="Max Lapan",
+    author_email="max.lapan@gmail.com",
+    license='GPL-v3',
+    description="PyTorch reinforcement learning framework",
     version="0.1",
 )
