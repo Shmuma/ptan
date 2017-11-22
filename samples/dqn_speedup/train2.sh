07e8cc2402671d7179e10d5ff0ce76bce29482e1
Max Lapan <max.lapan@gmail.com>
2017-11-22 20:10:45 +0100

    Train

--- samples/dqn_speedup/train2.sh
+++ samples/dqn_speedup/train2.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 while true; do
-    ./03_parallel.py --cuda -s 1
-    ./03_parallel.py --cuda -s 2
-    ./03_parallel.py --cuda -s 3
-    ./04_cuda_async.py --cuda -s 1
-    ./04_cuda_async.py --cuda -s 2
-    ./04_cuda_async.py --cuda -s 3
-    ./05_new_wrappers.py --cuda -s 1
-    ./05_new_wrappers.py --cuda -s 2
-    ./05_new_wrappers.py --cuda -s 3
+    ./03_parallel.py --cuda
+    ./04_cuda_async.py --cuda
+    ./05_new_wrappers.py --cuda
 done
