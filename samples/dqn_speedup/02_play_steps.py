a579b052602416a3df5ab7d382d82d6b3adbb0f9
Max Lapan <max.lapan@gmail.com>
2017-11-22 11:22:47 +0100

    Play steps

---
+++ samples/dqn_speedup/02_play_steps.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import gym
+import ptan
+import argparse
+
+import torch.optim as optim
+
+from tensorboardX import SummaryWriter
+
+from lib import dqn_model, common
+
+PLAY_STEPS = 2
+
+
+if __name__ == "__main__":
+    params = common.HYPERPARAMS['pong']
+    params['batch_size'] *= PLAY_STEPS
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    args = parser.parse_args()
+
+    env = gym.make(params['env_name'])
+    env = ptan.common.wrappers.wrap_dqn(env)
+
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-02_play_steps=%d" % PLAY_STEPS)
+    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
+    if args.cuda:
+        net.cuda()
+
+    tgt_net = ptan.agent.TargetNet(net)
+    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
+    epsilon_tracker = common.EpsilonTracker(selector, params)
+    agent = ptan.agent.DQNAgent(net, selector, cuda=args.cuda)
+
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params['replay_size'])
+    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
+
+    frame_idx = 0
+
+    with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
+        while True:
+            frame_idx += PLAY_STEPS
+            buffer.populate(PLAY_STEPS)
+            epsilon_tracker.frame(frame_idx)
+
+            new_rewards = exp_source.pop_total_rewards()
+            if new_rewards:
+                if reward_tracker.reward(new_rewards[0], frame_idx, selector.epsilon):
+                    break
+
+            if len(buffer) < params['replay_initial']:
+                continue
+
+            optimizer.zero_grad()
+            batch = buffer.sample(params['batch_size'])
+            loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
+            loss_v.backward()
+            optimizer.step()
+
+            if frame_idx % params['target_net_sync'] < PLAY_STEPS:
+                tgt_net.sync()
