c509a38b28ffd07e3a672579b5379e1d43b5f3e4
Max Lapan <max.lapan@gmail.com>
2017-11-22 22:07:37 +0100

    Timeouts, just in case

--- samples/dqn_speedup/train.sh
+++ samples/dqn_speedup/train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 while true; do
-    ./01_original.py --cuda
-    ./02_play_steps.py --cuda -s 2
-    ./02_play_steps.py --cuda -s 3
-    ./02_play_steps.py --cuda -s 4
+    timeout 1h ./01_original.py --cuda
+    timeout 1h ./02_play_steps.py --cuda -s 2
+    timeout 1h ./02_play_steps.py --cuda -s 3
+    timeout 1h ./02_play_steps.py --cuda -s 4
 done
--- samples/dqn_speedup/train2.sh
+++ samples/dqn_speedup/train2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 while true; do
-    ./03_parallel.py --cuda
-    ./04_cuda_async.py --cuda
-    ./05_new_wrappers.py --cuda
+    timeout 1h ./03_parallel.py --cuda
+    timeout 1h ./04_cuda_async.py --cuda
+    timeout 1h ./05_new_wrappers.py --cuda
 done
