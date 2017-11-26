ed8c2668f3ea19bd14ebcac10f1e50b3e83e65bc
Max Lapan <max.lapan@gmail.com>
2017-11-26 13:54:47 +0100

    Update QR starting point with the fastest version of DQN

--- samples/quantile_regression/dqn_atari.py
+++ samples/quantile_regression/dqn_atari.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import ptan
 import argparse
+
 import torch.optim as optim
 import torch.multiprocessing as mp
 
@@ -8,19 +9,19 @@ from tensorboardX import SummaryWriter
 
 from lib import dqn_model, common, atari_wrappers
 
-PLAY_STEPS = 3
+PLAY_STEPS = 4
 
 
 def make_env(params):
     env = atari_wrappers.make_atari(params['env_name'])
-    return atari_wrappers.wrap_deepmind(env, frame_stack=True, pytorch_img=True)
+    env = atari_wrappers.wrap_deepmind(env, frame_stack=True, pytorch_img=True)
+    return env
 
 
 def play_func(params, net, cuda, exp_queue):
     env = make_env(params)
 
     writer = SummaryWriter(comment="-" + params['run_name'] + "-dqn")
-
     selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
     epsilon_tracker = common.EpsilonTracker(selector, params)
     agent = ptan.agent.DQNAgent(net, selector, cuda=cuda)
@@ -42,6 +43,9 @@ def play_func(params, net, cuda, exp_queue):
                 if reward_tracker.reward(new_rewards[0], frame_idx, selector.epsilon):
                     break
 
+    exp_queue.put(None)
+
+
 if __name__ == "__main__":
     mp.set_start_method('spawn')
     params = common.HYPERPARAMS['pong']
@@ -51,11 +55,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     env = make_env(params)
-
     net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
     if args.cuda:
         net.cuda()
-    net.share_memory()
 
     tgt_net = ptan.agent.TargetNet(net)
 
@@ -69,18 +71,21 @@ if __name__ == "__main__":
     frame_idx = 0
 
     while play_proc.is_alive():
-        if not exp_queue.empty():
-            for _ in range(PLAY_STEPS):
-                new_exp = exp_queue.get()
-                buffer._add(new_exp)
-                frame_idx += 1
+        frame_idx += PLAY_STEPS
+        for _ in range(PLAY_STEPS):
+            exp = exp_queue.get()
+            if exp is None:
+                play_proc.join()
+                break
+            buffer._add(exp)
 
         if len(buffer) < params['replay_initial']:
             continue
 
         optimizer.zero_grad()
         batch = buffer.sample(params['batch_size'])
-        loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
+        loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'],
+                                      cuda=args.cuda, cuda_async=True)
         loss_v.backward()
         optimizer.step()
 
--- samples/quantile_regression/qr_atari.py
+++ samples/quantile_regression/qr_atari.py
@@ -1,44 +1,41 @@
 #!/usr/bin/env python3
-import gym
 import ptan
 import argparse
+
 import torch.optim as optim
+import torch.multiprocessing as mp
 
 from tensorboardX import SummaryWriter
 
-from lib import dqn_model, common
+from lib import dqn_model, common, atari_wrappers
+
+PLAY_STEPS = 4
 
 
-if __name__ == "__main__":
-    params = common.HYPERPARAMS['pong']
-#    params['epsilon_frames'] = 200000
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
-    args = parser.parse_args()
+def make_env(params):
+    env = atari_wrappers.make_atari(params['env_name'])
+    env = atari_wrappers.wrap_deepmind(env, frame_stack=True, pytorch_img=True)
+    return env
 
-    env = gym.make(params['env_name'])
-    env = ptan.common.wrappers.wrap_dqn(env)
+
+def play_func(params, net, cuda, exp_queue):
+    env = make_env(params)
 
     writer = SummaryWriter(comment="-" + params['run_name'] + "-qr")
-    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
-    if args.cuda:
-        net.cuda()
-
-    tgt_net = ptan.agent.TargetNet(net)
     selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
     epsilon_tracker = common.EpsilonTracker(selector, params)
-    agent = ptan.agent.DQNAgent(net, selector, cuda=args.cuda)
-
+    agent = ptan.agent.DQNAgent(net, selector, cuda=cuda)
     exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
-    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params['replay_size'])
-    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
+    exp_source_iter = iter(exp_source)
 
     frame_idx = 0
 
     with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
         while True:
             frame_idx += 1
-            buffer.populate(1)
+            exp = next(exp_source_iter)
+            exp_queue.put(exp)
+
             epsilon_tracker.frame(frame_idx)
 
             new_rewards = exp_source.pop_total_rewards()
@@ -46,14 +43,51 @@ if __name__ == "__main__":
                 if reward_tracker.reward(new_rewards[0], frame_idx, selector.epsilon):
                     break
 
-            if len(buffer) < params['replay_initial']:
-                continue
+    exp_queue.put(None)
 
-            optimizer.zero_grad()
-            batch = buffer.sample(params['batch_size'])
-            loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
-            loss_v.backward()
-            optimizer.step()
 
-            if frame_idx % params['target_net_sync'] == 0:
-                tgt_net.sync()
+if __name__ == "__main__":
+    mp.set_start_method('spawn')
+    params = common.HYPERPARAMS['pong']
+    params['batch_size'] *= PLAY_STEPS
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    args = parser.parse_args()
+
+    env = make_env(params)
+    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
+    if args.cuda:
+        net.cuda()
+
+    tgt_net = ptan.agent.TargetNet(net)
+
+    buffer = ptan.experience.ExperienceReplayBuffer(experience_source=None, buffer_size=params['replay_size'])
+    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
+
+    exp_queue = mp.Queue(maxsize=PLAY_STEPS * 2)
+    play_proc = mp.Process(target=play_func, args=(params, net, args.cuda, exp_queue))
+    play_proc.start()
+
+    frame_idx = 0
+
+    while play_proc.is_alive():
+        frame_idx += PLAY_STEPS
+        for _ in range(PLAY_STEPS):
+            exp = exp_queue.get()
+            if exp is None:
+                play_proc.join()
+                break
+            buffer._add(exp)
+
+        if len(buffer) < params['replay_initial']:
+            continue
+
+        optimizer.zero_grad()
+        batch = buffer.sample(params['batch_size'])
+        loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'],
+                                      cuda=args.cuda, cuda_async=True)
+        loss_v.backward()
+        optimizer.step()
+
+        if frame_idx % params['target_net_sync'] < PLAY_STEPS:
+            tgt_net.sync()
