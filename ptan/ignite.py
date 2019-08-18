efc24f799ebfdf55190ce0c8dd7598fe840837e7
Max Lapan <max.lapan@gmail.com>
2019-08-18 09:46:11 +0300

    Option to subsample episodes

--- ptan/ignite.py
+++ ptan/ignite.py
@@ -14,10 +14,19 @@ class EpisodeEvents(enum.Enum):
 
 class EndOfEpisodeHandler:
     def __init__(self, exp_source: ptan.experience.ExperienceSource, alpha: float = 0.98,
-                 bound_avg_reward: Optional[float] = None):
+                 bound_avg_reward: Optional[float] = None,
+                 subsample_end_of_episode: Optional[int] = None):
+        """
+        Construct end-of-episode event handler
+        :param exp_source: experience source to use
+        :param alpha: smoothing alpha param
+        :param bound_avg_reward: optional boundary for average reward
+        :param subsample_end_of_episode: if given, end of episode event will be subsampled by this amount
+        """
         self._exp_source = exp_source
         self._alpha = alpha
         self._bound_avg_reward = bound_avg_reward
+        self._subsample_end_of_episode = subsample_end_of_episode
 
     def attach(self, engine: Engine):
         engine.add_event_handler(Events.ITERATION_COMPLETED, self)
@@ -33,7 +42,8 @@ class EndOfEpisodeHandler:
             engine.state.metrics['reward'] = reward
             engine.state.metrics['steps'] = steps
             self._update_smoothed_metrics(engine, reward, steps)
-            engine.fire_event(EpisodeEvents.EPISODE_COMPLETED)
+            if self._subsample_end_of_episode is None or engine.state.episode % self._subsample_end_of_episode == 0:
+                engine.fire_event(EpisodeEvents.EPISODE_COMPLETED)
             if self._bound_avg_reward is not None and engine.state.metrics['avg_reward'] >= self._bound_avg_reward:
                 engine.fire_event(EpisodeEvents.BOUND_REWARD_REACHED)
 
