df55558703d8da26f0446121ebf0564d4b0d621b
Dan Carter <daniel.carter@unity3d.com>
2023-09-20 16:35:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PTAN stands for PyTorch AgentNet -- reimplementation of AgentNet library for pyt
 import setuptools
 
 
-requirements = ['torch==1.7.0', 'gym', 'atari-py', 'numpy', 'opencv-python']
+requirements = ['torch==1.11.0', 'gym', 'atari-py', 'numpy', 'opencv-python']
 
 
 setuptools.setup(
