4f948ef6a0375455438f6a26fa4b8b2d9cb4345e
Max Lapan <max.lapan@gmail.com>
2024-02-16 20:19:19 +0100

    More tests

--- ptan/agent.py
+++ ptan/agent.py
@@ -1,55 +1,60 @@
 """
 Agent is something which converts states into actions and has state
 """
+import abc
 import copy
 import numpy as np
 import torch
+from torch import nn
 import torch.nn.functional as F
+import typing as tt
 
 from . import actions
 
+States = tt.List[np.ndarray]
+AgentStates = tt.List[tt.Any]
+Preprocessor = tt.Callable[[States], torch.Tensor]
 
-class BaseAgent:
+
+class BaseAgent(abc.ABC):
     """
     Abstract Agent interface
     """
-    def initial_state(self):
+    def initial_state(self) -> tt.Optional[tt.Any]:
         """
-        Should create initial empty state for the agent. It will be called for the start of the episode
+        Should create initial empty state for the agent. It will be
+        called for the start of the episode
         :return: Anything agent want to remember
         """
         return None
 
-    def __call__(self, states, agent_states):
+    @abc.abstractmethod
+    def __call__(self, states: States, agent_states: AgentStates):
         """
         Convert observations and states into actions to take
         :param states: list of environment states to process
         :param agent_states: list of states with the same length as observations
         :return: tuple of actions, states
         """
-        assert isinstance(states, list)
-        assert isinstance(agent_states, list)
-        assert len(agent_states) == len(states)
-
-        raise NotImplementedError
+        ...
 
 
-def default_states_preprocessor(states):
+def default_states_preprocessor(states: States) -> torch.Tensor:
     """
-    Convert list of states into the form suitable for model. By default we assume Variable
+    Convert list of states into the form suitable for model. By default we assume torch.Tensor
     :param states: list of numpy arrays with states
-    :return: Variable
+    :return: torch.Tensor
     """
     if len(states) == 1:
         np_states = np.expand_dims(states[0], 0)
     else:
         np_states = np.array([np.array(s, copy=False) for s in states], copy=False)
-    return torch.tensor(np_states)
+    return torch.as_tensor(np_states)
 
 
-def float32_preprocessor(states):
+def float32_preprocessor(states: States):
     np_states = np.array(states, dtype=np.float32)
-    return torch.tensor(np_states)
+    return torch.as_tensor(np_states)
 
 
 class DQNAgent(BaseAgent):
@@ -57,14 +62,16 @@ class DQNAgent(BaseAgent):
     DQNAgent is a memoryless DQN agent which calculates Q values
     from the observations and  converts them into the actions using action_selector
     """
-    def __init__(self, dqn_model, action_selector, device="cpu", preprocessor=default_states_preprocessor):
+    def __init__(self, dqn_model: nn.Module, action_selector: actions.ActionSelector,
+                 device: torch.device = torch.device("cpu"),
+                 preprocessor: Preprocessor = default_states_preprocessor):
         self.dqn_model = dqn_model
         self.action_selector = action_selector
         self.preprocessor = preprocessor
         self.device = device
 
     @torch.no_grad()
-    def __call__(self, states, agent_states=None):
+    def __call__(self, states: States, agent_states: tt.Optional[AgentStates] = None):
         if agent_states is None:
             agent_states = [None] * len(states)
         if self.preprocessor is not None:
@@ -81,7 +88,7 @@ class TargetNet:
     """
     Wrapper around model which provides copy of it instead of trained weights
     """
-    def __init__(self, model):
+    def __init__(self, model: nn.Module):
         self.model = model
         self.target_model = copy.deepcopy(model)
 
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,52 +1,90 @@
-import pytest
+import collections
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 import ptan.actions as actions
 
 
 def test_argmax():
     sel = actions.ArgmaxActionSelector()
-    with pytest.raises(AssertionError):
-        sel(1)
-    with pytest.raises(AssertionError):
-        sel([1, 2])
-
     s = np.array([[1, 2], [0, -10], [0.01, 0.1]])
     r = sel(s)
     npt.assert_equal(r, [1, 0, 1])
-#
-#
-# class TestEpsilonGreedyActionSelector(TestCase):
-#     def test_deterministic(self):
-#         selector = actions.EpsilonGreedyActionSelector(epsilon=0.0)
-#         r = selector(np.array(
-#             [[1.0, 0.0],
-#              [0.0, 1.0],
-#              [0.2, 0.8]]))
-#         np.testing.assert_equal(r, [0, 1, 1])
-#
-#     def test_random(self):
-#         selector = actions.EpsilonGreedyActionSelector(epsilon=1.0)
-#         r = selector(np.array(
-#             [[1.0, 0.0],
-#              [0.0, 1.0],
-#              [0.2, 0.8]]))
-#         self.assertEqual(r.shape, (3, ))
-#
-#         # test stochastic
-#         selector = actions.EpsilonGreedyActionSelector(epsilon=0.5)
-#         count_total = 0
-#         count_one = 0
-#         for _ in range(1000):
-#             r = selector(np.array(
-#                 [[1.0, 0.0],
-#                  [1.0, 0.0],
-#                  [1.0, 0.0],
-#                  [1.0, 0.0]]))
-#             count_total += r.shape[0]
-#             count_one += np.sum(r)
-#         eps = abs(count_one*4 / count_total - 1.)
-#         self.assertTrue(eps < 0.1)
-#
-#
+
+
+def test_epsilon_deterministic():
+    selector = actions.EpsilonGreedyActionSelector(epsilon=0.0)
+    s = np.array(
+        [[1.0, 0.0],
+         [0.0, 1.0],
+         [0.2, 0.8]])
+    r = selector(s)
+    npt.assert_equal(r, [0, 1, 1])
+
+    selector.epsilon = 1.0
+    with pytest.raises(ValueError):
+        selector.epsilon = 1.1
+
+
+def test_epsilon_random():
+    selector = actions.EpsilonGreedyActionSelector(epsilon=1.0)
+    r = selector(np.array(
+        [[1.0, 0.0],
+         [0.0, 1.0],
+         [0.2, 0.8]]))
+    assert r.shape == (3, )
+
+    # test stochastic
+    selector = actions.EpsilonGreedyActionSelector(epsilon=0.5)
+    count_total = 0
+    count_one = 0
+    for _ in range(1000):
+        r = selector(np.array(
+            [[1.0, 0.0],
+             [1.0, 0.0],
+             [1.0, 0.0],
+             [1.0, 0.0]]))
+        count_total += r.shape[0]
+        count_one += np.sum(r)
+    eps = abs(count_one*4 / count_total - 1.)
+    assert eps < 0.1
+
+
+def test_prob_action_sel():
+    sel = actions.ProbabilityActionSelector()
+    stats = [
+        collections.Counter()
+        for _ in range(3)
+    ]
+
+    for _ in range(1000):
+        acts = sel(np.array([
+            [0.1, 0.8, 0.1],
+            [0.0, 0.0, 1.0],
+            [0.5, 0.5, 0.0]
+        ]))
+        for idx, act in enumerate(acts):
+            stats[idx][act] += 1
+
+    assert 0.7 < stats[0][1] / 1000 < 0.9
+    assert stats[1][2] == 1000
+    assert stats[2][2] == 0
+    assert 0.4 < stats[2][0] / 1000 < 0.6
+
+
+def test_epsilon_tracker():
+    tracker = actions.EpsilonTracker(
+        actions.EpsilonGreedyActionSelector(),
+        1.0, 0.0,
+        100
+    )
+    assert tracker.selector.epsilon == 1.0
+    tracker.frame(10)
+    assert round(tracker.selector.epsilon, 1) == 0.9
+    tracker.frame(90)
+    assert round(tracker.selector.epsilon, 1) == 0.1
+    tracker.frame(100)
+    assert tracker.selector.epsilon == 0.0
+    tracker.frame(110)
+    assert tracker.selector.epsilon == 0.0
